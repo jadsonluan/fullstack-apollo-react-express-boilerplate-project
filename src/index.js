e96f4a26bf8427410a58f5b66757c556ba7e37b6
Jadson Luan <jadsonluan1@gmail.com>
2019-04-20 19:16:42 -0300

    Initial setup (with Express)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-import 'dotenv/config';
-console.log('Hello ever running Node.js project.');
-console.log(process.env.MY_SECRET);
+import cors from 'cors';
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+const app = express();
+
+app.use(cors());
+
+const schema = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    username: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: 'Jadson Luan',
+      };
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+})
+
+server.applyMiddleware({ app, path: '/graphql'});
+
+app.listen({ port: 8000}, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql');
+})
